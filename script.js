ac01741eadf35894d7a2151b60b5b7075e5db830
Vishwesh Shukla <vishveshshukla82@gmail.com>
2025-12-19 10:52:09 +0530

    Selection-sort

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-function bubbleSort(arr) {
-    let it = 0;
-
-    while (it < arr.length - 1) {
-        let swapped = false;
-
-        for (let i = 0; i < arr.length - 1 - it; i++) {
-            if (arr[i] > arr[i + 1]) {
-                let temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-                swapped = true;
+function InsertionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+                isSwapped = true;
             }
         }
-
-        if (!swapped) break;
-        it++;
+        let temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
     }
 
-    console.log(arr);
+    console.log(arr)
 }
 
-bubbleSort([3, 8, 4, 1, 7]);
+
+InsertionSort([3, 2, 4, 5, 1, 0, 7])
