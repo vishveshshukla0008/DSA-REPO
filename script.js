9a92d08888caff18ec88fc5997bb4654eafbb9e9
Vishwesh Shukla <vishveshshukla82@gmail.com>
2026-01-04 14:45:46 +0530

    Rotate-image-meddium-level

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
-/**
- * @param {string} s
- * @param {string} target
- * @return {number}
- */
-var rearrangeCharacters = function (s, target) {
-    let freqS = {};
-    let freqT = {};
-
-    // frequency of s
-    for (let ch of s) {
-        freqS[ch] = (freqS[ch] || 0) + 1;
-    }
-
-    // frequency of target
-    for (let ch of target) {
-        freqT[ch] = (freqT[ch] || 0) + 1;
-    }
-
-    console.log(freqS)
-    console.log(freqT)
-        let min = Infinity;
-    
-        for (let ch in freqT) {
-            if (!freqS[ch]) return 0;
-            min = Math.min(min, Math.floor(freqS[ch] / freqT[ch]));
+var rotate = function(matrix) {
+    // 1. Find the transpose :
+    for(let i=0; i<matrix.length; i++) {
+        for(let j=i;j<matrix[i].length;j++) {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
         }
-    
-        return min;
-};  
+    }
 
 
-console.log(rearrangeCharacters("ilovecodingonleetcode", "code"));
+    // 2 . Flip the Matrix
+};
+
+rotate([[1,2,3],[4,5,6],[7,8,9]]);
