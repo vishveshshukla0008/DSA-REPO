02292802186d8832b5955e2e5192681ce5bce9e4
vishveshshukla0008 <vishveshshukla82@gmail.com>
2026-02-22 20:42:17 +0530

    BIT MANIPULATION

--- app.js
+++ app.js
@@ -1,48 +1,63 @@
-var setZeroes = function (matrix) {
-    // Iterate on matrix :
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[i].length; j++) {
-            if (matrix[i][j] == 0) {
-                makeRelatedRowColumnZero(matrix, i, j);
-            }
-        }
-    }
+// check the ith bit is 1 or 0
 
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[i].length; j++) {
-            if (matrix[i][j] == -10) {
-                matrix[i][j] = 0
-            }
-        }
-    }
-
-    console.log(matrix)
-};
-
-
-function makeRelatedRowColumnZero(matrix, i, j) {
-    // make row to -10 :
-    let scol = 0;
-    let endCol = matrix[i].length - 1;
-
-    let srow = 0;
-    let endRow = matrix.length - 1;
-
-
-    for (let k = scol; k <= endCol; k++) {
-        if (matrix[i][k] != 0) {
-
-            matrix[i][k] = -10;
-        }
-    }
-
-    for (let s = 0; s <= endRow; s++) {
-        if (matrix[s][j] != 0) {
-            matrix[s][j] = -10;
-        }
-    }
+function getIthBit(num, i) {
+    console.log((num >> i) & 1);
 }
 
 
+getIthBit(13, 2);
 
-setZeroes([[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]);
+
+function checkNumberIsEvenOrOdd(num) {
+    // find the last bit only 
+
+    // if 1 -> then odd 
+    console.log((num & 1) == 0 ? "even" : "odd")
+
+    // if 0 -> then even
+}
+
+checkNumberIsEvenOrOdd(2) // 0
+checkNumberIsEvenOrOdd(3) // 1
+checkNumberIsEvenOrOdd(4) //0
+
+
+//swap the numbers using bits :
+
+function swapUsingBits(a, b) {
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    console.log(`a = ${a} || b = ${b}`)
+}
+
+swapUsingBits(10, 14)
+
+
+// flip the bit if 1 then 0 or viceversa :
+
+function flipBit(num, i) {
+    const mask = 1 << i;
+    console.log(num & mask)
+}
+
+flipBit(10, 1)
+
+
+// check the number is the power of two or not  :
+
+function chcekInPowerOfTwo(num) {
+
+    // 32 => 10000
+    // 31 => 01111
+
+    //8 => 1000
+    //7 => 0111
+
+    // for power of n it is must to msb 1 and all are 0 so if result of num & prev == 0 true otherwise false
+    return num & (num - 1) ? false : true;
+}
+
+console.log(chcekInPowerOfTwo(8));
+
+
