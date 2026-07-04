3e317a7da13ae48e149d99a6a580c72ed98cee5e
vishveshshukla0008 <vishveshshukla82@gmail.com>
2026-07-04 16:06:44 +0530

    Queue implementation !

--- index.js
+++ index.js
@@ -1,194 +1,65 @@
 class Node {
-  constructor(data, next) {
+  constructor(data) {
     this.data = data;
     this.next = null;
   }
 }
 
-class LL {
+class Queue {
+  size = 0;
   constructor() {
-    this.head = null; // pointer jo ki point karega starting ko
-    this.size = 0;
+    this.head = null;
   }
 
-  insertAtStart(value) {
-    let node = new Node(value);
-    this.size++;
-    if (this.head == null) {
-      this.head = node;
+  dequeue() {
+    if (!this.head) {
+      console.log("The entire queue is empty !");
       return;
     }
-
-    node.next = this.head;
-    this.head = node;
+    this.size--;
+    this.head = this.head.next;
   }
-
-  insertAtEnd(value) {
-    let node = new Node(value);
+  enqueue(val) {
+    let newNode = new Node(val);
     this.size++;
-    if (this.head == null) {
-      this.head = node;
+    if (this.head === null) {
+      this.head = newNode;
       return;
     }
 
     let temp = this.head;
-
     while (temp.next !== null) {
       temp = temp.next;
     }
 
-    temp.next = node;
-  }
-
-  deleteAtStart() {
-    if (this.head == null) {
-      console.log("Linked List has already empty ! can't delete !");
-      return;
-    }
-
-    this.head = this.head.next;
-    this.size--;
-  }
-
-  deleteAtLast() {
-    if (this.head === null) {
-      console.log("Linked list has already empty !");
-    }
-
-    let temp = this.head;
-
-    while (temp.next.next !== null) {
-      temp = temp.next;
-    }
-
-    temp.next = null;
-    this.size--;
-  }
-
-  insertAtPosition(position, data) {
-    // check the list is empty and inserting position!
-
-    if (position < 1 || position > this.size + 1) {
-      console.log("Position is not valid !");
-      return;
-    }
-
-    if (position === 1) {
-      this.insertAtStart(data);
-      return;
-    }
-
-    if (position === this.size + 1) {
-      this.insertAtEnd(data);
-      return;
-    }
-
-    let count = 1;
-
-    let temp = this.head;
-
-    while (count < position - 1) {
-      temp = temp.next;
-      count++;
-    }
-
-    let opt = temp.next;
-
-    let newNode = new Node(data);
-
-    newNode.next = opt;
     temp.next = newNode;
-
-    this.size++;
   }
 
-  deleteAtPosition(index) {
-    if (index < 0 || index >= this.size) return;
-
-    if (index == 0) {
-      this.head = this.head.next;
-      this.size--;
+  printQueue() {
+    if (!this.head) {
+      console.log("The entire queue is empty !");
       return;
     }
-
-    let count = 1;
-
+    process.stdout.write("\n");
     let temp = this.head;
-    for (let i = 0; i < index - 1; i++) {
-      temp = temp.next; // traversing !
-    }
-    temp.next = temp.next.next;
-  }
-
-  printList() {
-    // check karo ki kya list hai bi
-    if (this.head === null) {
-      console.log("Linked List is empty !");
-      return;
-    }
-
-    let temp = this.head;
-    while (temp !== null) {
-      process.stdout.write(`${temp.data}->`);
+    while (temp) {
+      process.stdout.write(`${temp.data} -> `);
       temp = temp.next;
     }
     process.stdout.write("null");
-    console.log();
-  }
-
-  getIndexValue(index) {
-    if (index < 0 || index >= this.size) return -1;
-    if (index == 0) {
-      console.log(this.head.data);
-      return;
-    }
-    let temp = this.head;
-    let count = 1;
-    while (count < index) {
-      temp = temp.next;
-      count++;
-    }
-
-    console.log(temp.next);
-  }
-
-  findMid() {
-    if (this.size == 0) {
-      console.log("Empty Linked list !");
-      return;
-    }
-
-    if (this.size == 1) {
-      console.log(this.head.data);
-      return;
-    }
-
-    // bruteforce approach !
-
-    let mid = Math.floor(this.size / 2);
-
-    let count = 1;
-    let temp = this.head;
-
-    while(count <= mid) {
-      temp = temp.next;
-      count++;
-    }
-
-    console.log(temp.data)
+    process.stdout.write("\n");
   }
 }
 
-const list = new LL();
-list.insertAtEnd(10);
-list.insertAtEnd(20);
-list.insertAtEnd(30);
-list.insertAtEnd(40);
-list.insertAtEnd(60);
-list.insertAtEnd(70);
-// list.insertAtEnd(60);
+const q = new Queue();
+q.enqueue(10);
+q.enqueue(20);
+q.enqueue(30);
+q.enqueue(40);
+q.enqueue(50);
 
-list.printList();
-console.log(list.size);
+q.printQueue();
 
-list.findMid();
+q.dequeue();
+q.printQueue();
+console.log(q.size)
