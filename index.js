7363cd9a3545d3a35cfcbde0436d848a19e319df
vishveshshukla0008 <vishveshshukla82@gmail.com>
2026-07-08 16:27:23 +0530

    Fact

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-// Remove Duplicates from array !
+let num = 3;
 
-let arr = [1,2,8,2,3,4,4,4,5,6,7];
-
-function removeDuplicates(arr) {
-    let ans = [];
-    arr.sort();
-    for(let i=0; i<arr.length; i++) {
-        if(arr[i] !== arr[i-1]) {
-            ans.push(arr[i]);
-        }
+function findFact(n) {
+    if(n == 0 || n == 1) return 1;
+    let ans = 1;
+    for(let i=2; i<= n; i++) {
+        ans *= i;
     }
-    console.log(ans);
+
     return ans;
 }
 
-removeDuplicates(arr)
+console.log(findFact(num))
