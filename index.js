0d5c13d8f1905711aba3f45c29538df5d4c7e2fc
vishveshshukla0008 <vishveshshukla82@gmail.com>
2026-07-08 16:06:05 +0530

    Check pailidrome

--- index.js
+++ index.js
@@ -1,68 +1,17 @@
-class Stack {
-  constructor() {
-    this.peak = null;
-    this.arr = [];
-  }
+str = "cababac";
 
-  push(val) {
-    this.arr.push(val);
-    this.peak = this.arr.length - 1;
-  }
+function checkPalindrome(s) {
+    let i=0, j= str.length-1;
 
-  pop() {
-    if (this.arr.length === 0) {
-      console.log("Stack is empty !");
-      return;
-    }
-    const popElement = this.arr.pop();
-    this.peak = this.arr.length - 1;
-    return popElement;
-  }
-
-  getPeak() {
-    if (this.arr[this.peak] == undefined) {
-      console.log("Stack is empty !");
-      return null;
+    while(i != j) {
+        if(s[i] !== s[j]) {
+            return false;
+        }
+        i++;
+        j--;
     }
 
-    return this.arr[this.peak];
-  }
-
-  print() {
-    let i = this.peak;
-    while (i >= 0) {
-      process.stdout.write(`${this.arr[i]} -> `);
-      i--;
-    }
-    console.log();
-  }
+    return true;
 }
 
-function reverseStack(stack) {
-  let s = new Stack();
-  let i = stack.peak;
-  while (stack.peak >= 0) {
-    s.push(stack.pop());
-  }
-
-  return s;
-}
-
-const s = new Stack();
-s.push(10);
-s.push(20);
-s.push(30);
-s.push(40);
-
-s.print();
-//
-// s.pop();
-// s.pop();
-// s.pop();
-// s.pop();
-// s.print();
-
-const s2 = reverseStack(s);
-s2.print();
-
-console.log(s2.getPeak());
+console.log(checkPalindrome(str));
